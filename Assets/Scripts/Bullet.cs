2dd1e5a3afac8053c52f66a33074df4f7061860c
Alayas Hu <alayashu9@gmail.com>
2026-03-03 22:14:53 -0600

    init

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	[SerializeField] private float speed = 10f;
+    [SerializeField] private float lifetime = 3f;
+
+    private Vector2 direction;
+
+    public void Init(Vector2 dir) {
+        direction = dir.normalized;
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update() {
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Enemy")) {
+			Debug.Log("Hit");
+			Destroy(gameObject);
+			Destroy(other.gameObject); // Change to damage and not instant destroy
+		}
+		else if (other.CompareTag("Collision")) {
+			Destroy(gameObject);
+		}
+    }
+
+	public void Init(Vector2 dir, Collider2D shooterCollider) {
+    	direction = dir.normalized;
+    	Destroy(gameObject, lifetime);
+
+	    if (shooterCollider != null)
+   			Physics2D.IgnoreCollision(GetComponent<Collider2D>(), shooterCollider);
+}
+
+}
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+
+public class SaveData {
+	public Vector3 playerPosition;
+	public string mapBoundary; //Boundry name for the map
+	public List<InventorySaveData> inventorySaveData;
+
+}
