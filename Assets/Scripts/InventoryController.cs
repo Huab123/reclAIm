d3e5dcbbeca4d9c3e1d4e3dcfa498db86bb16424
Alayas Hu <alayashu9@gmail.com>
2026-03-07 02:12:36 -0600

    add Item pickup

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -9,6 +9,21 @@ public class InventoryController : MonoBehaviour {
 	public int slotCount;
 	public GameObject[] itemPrefabs;
 
+	public bool AddItem(GameObject itemPrefab) {
+		foreach(Transform slotTransform in inventoryPanel.transform) {
+			Slot slot = slotTransform.GetComponent<Slot>();
+			if (slot != null && slot.currentItem == null) {
+				GameObject newItem = Instantiate(itemPrefab, slotTransform);
+				newItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+				slot.currentItem = newItem;
+				return true;
+			}
+		}
+
+		Debug.Log("Inventory is full!");
+		return false;
+	}
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
 		itemDictionary = FindFirstObjectByType<ItemDictionary>();
