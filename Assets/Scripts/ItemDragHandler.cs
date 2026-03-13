04b9fcdf418b3ab3160ca5c8462f8c614a05d0ae
Alayas Hu <alayashu9@gmail.com>
2026-03-12 22:06:21 -0500

    add Drop item

--- Assets/Scripts/ItemDragHandler.cs
+++ Assets/Scripts/ItemDragHandler.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
 
-public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
-{
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 	Transform originalParent;
 	CanvasGroup canvasGroup;
 
@@ -37,6 +37,7 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
 		Slot originalSlot = originalParent.GetComponent<Slot>();
 		if (dropSlot != null) {
+			// If the drop slot already has an item, swap them
 			if (dropSlot.currentItem != null) {
 
 				dropSlot.currentItem.transform.SetParent(originalSlot.transform);
@@ -46,15 +47,43 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 			else {
 				originalSlot.currentItem = null;
 			}
-
+			// Place the dragged item in the new slot
 			transform.SetParent(dropSlot.transform);
 			dropSlot.currentItem = gameObject;
 		}
 		else {
-			transform.SetParent(originalParent);
+			if(!IsWithinInventory(eventData.position)) {
+				// Drop the item outside the inventory (e.g., into the world)
+				DropItem(originalSlot, eventData.position);
+			}
+			else {
+				// Return to original slot if dropped outside any slot but within inventory
+				transform.SetParent(originalParent);
+			}
 		}
 
 		GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
 
+	}
+
+	bool IsWithinInventory(Vector2 mousePosition) {
+		RectTransform inventoryRect = originalParent.GetComponentInParent<RectTransform>();
+		return RectTransformUtility.RectangleContainsScreenPoint(inventoryRect, mousePosition);
+	}
+
+	void DropItem(Slot originalSlot, Vector2 screenPosition){
+		originalSlot.currentItem = null;
+
+		// Convert screen position to world point on Z=0 plane
+		Camera cam = Camera.main;
+		Vector3 worldPos = cam.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, cam.nearClipPlane));
+		worldPos.z = 0f; // ensure 2D plane
+
+		// Instantiate the dropped item in the world
+		GameObject dropItem = Instantiate(gameObject, worldPos, Quaternion.identity);
+		dropItem.GetComponent<BounceEffect>().StartBounce();
+
+		// Destroy UI item
+		Destroy(gameObject);
 	}
 }
--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,13 +6,13 @@ public class MenuController : MonoBehaviour
 	public GameObject menuCanvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-		menuCanvas.SetActive(false);        
+		  menuCanvas.SetActive(false);        
     }
 
     // Update is called once per frame
     void Update() {
-       	if (Keyboard.current.escapeKey.wasPressedThisFrame) {
-			menuCanvas.SetActive(!menuCanvas.activeSelf);
-		}
+      if (Keyboard.current.escapeKey.wasPressedThisFrame) {
+			  menuCanvas.SetActive(!menuCanvas.activeSelf);
+		  }
     }
 }
