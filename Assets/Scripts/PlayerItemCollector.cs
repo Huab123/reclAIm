4036400f9b140a47d0e1c7e4fcc7a60d1521222b
Yuanzhe Ni <1212starxx@gmail.com>
2026-03-26 14:36:25 -0500

    add sound

--- Assets/Scripts/PlayerItemCollector.cs
+++ Assets/Scripts/PlayerItemCollector.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using UnityEngine;
 
 public class PlayerItemCollector : MonoBehaviour
@@ -32,3 +33,39 @@ public class PlayerItemCollector : MonoBehaviour
 		}
 	}
 }
+=======
+using UnityEngine;
+
+public class PlayerItemCollector : MonoBehaviour
+{
+	private InventoryController inventoryController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+       	inventoryController = FindFirstObjectByType<InventoryController>(); 
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.CompareTag("Item")) {
+			Item item = collision.GetComponent<Item>();
+			if (item != null && !item.IsPickedUp) {
+				// Avoid double-collect on multi-collider/player-overlap
+				Collider2D col = collision.GetComponent<Collider2D>();
+				if (col != null) col.enabled = false;
+
+				bool itemAdded = inventoryController.AddItem(item.ID);
+				if (itemAdded) {
+					if (SoundEffectManager.Instance != null)
+                    {
+                        SoundEffectManager.Instance.PlayPotionPickup();
+                    }	
+					item.Pickup();
+					Destroy(collision.gameObject);
+				} else {
+					if (col != null) col.enabled = true;
+				}
+			}
+		}
+	}
+}
+>>>>>>> Stashed changes
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -48,3 +49,55 @@ public class Weapon : MonoBehaviour
         }
     }
 }
+=======
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Weapon : MonoBehaviour
+{
+	[SerializeField] private float orbitRadius = 1f;
+	private Transform playerPos;
+	[SerializeField] private GameObject bulletPrefab;
+	[SerializeField] private GameObject player;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float fireRate = 0.2f;
+
+    private float nextFireTime = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+	    player = GameObject.FindGameObjectWithTag("Player");
+       	playerPos = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(
+        new Vector3(Mouse.current.position.ReadValue().x, 
+                    Mouse.current.position.ReadValue().y, 0f));
+    	mouseWorld.z = 0f;
+
+    	Vector2 direction = (mouseWorld - playerPos.position).normalized;
+    	transform.position = playerPos.position + (Vector3)(direction * orbitRadius);
+
+    	float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    	transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+	public void Fire() {
+		if (Time.time < nextFireTime) return;
+        nextFireTime = Time.time + (fireRate / PlayerStats.Instance.attackSpeedMult); //this is where modding attack speed needs to go
+
+        // Spawn bullet at firepoint
+        GameObject bulletObj = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        // Pass direction to bullet (gun's right = forward since sprite points right)
+        bulletObj.GetComponent<Bullet>().Init(firePoint.right);
+        if (SoundEffectManager.Instance != null)
+        {
+            SoundEffectManager.Instance.PlayGunShot();
+        }
+    }
+}
+>>>>>>> Stashed changes
