00e26dc6841ba1fb27213e55f2237d51c4ef27f5
viperOnCrit <96153001+viperOnCrit@users.noreply.github.com>
2026-03-26 14:46:08 -0500

    fixed merge errors

--- Assets/Scripts/SoundEffectManager.cs
+++ Assets/Scripts/SoundEffectManager.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using UnityEngine;
 
 public class SoundEffectManager : MonoBehaviour
@@ -48,55 +47,4 @@ public class SoundEffectManager : MonoBehaviour
             audioSource.PlayOneShot(potionPickupSound, potionPickupVolume * masterSFXVolume);
         }
     }
-=======
-using UnityEngine;
-
-public class SoundEffectManager : MonoBehaviour
-{
-    public static SoundEffectManager Instance;
-
-    [Header("Sound Effects")]
-    public AudioClip gunShotSound;
-    public AudioClip potionPickupSound;
-
-    [Header("Volume Settings")]
-    [Range(0f, 1f)] public float masterSFXVolume = 1f;
-    [Range(0f, 1f)] public float gunShotVolume = 0.3f;
-    [Range(0f, 1f)] public float potionPickupVolume = 0.6f;
-
-    private AudioSource audioSource;
-
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        Instance = this;
-        audioSource = GetComponent<AudioSource>();
-
-        if (audioSource == null)
-        {
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
-    }
-
-    public void PlayGunShot()
-    {
-        if (gunShotSound != null)
-        {
-            audioSource.PlayOneShot(gunShotSound, gunShotVolume * masterSFXVolume);
-        }
-    }
-
-    public void PlayPotionPickup()
-    {
-        if (potionPickupSound != null)
-        {
-            audioSource.PlayOneShot(potionPickupSound, potionPickupVolume * masterSFXVolume);
-        }
-    }
->>>>>>> Stashed changes
 }
